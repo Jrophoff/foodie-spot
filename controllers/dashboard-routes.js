b0c55ce03ee01ea5fd732a0df9358ab5f352a9a1
brt9029 <brt9029@gmail.com>
2022-01-11 21:36:47 -0600

    base controllers added/created

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const withAuth = require('../utils/auth');
+
+module.exports = router;
---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS foodiespot_db;
+
+CREATE DATABASE foodiespot_db;
